e0217731793824ddcbff66e945348ec99bcdf6f1
acutuc <acutuc94@gmail.com>
2022-09-28 12:29:59 +0200

    Probando Git en VS

---
+++ Tema1/Arrays/Ejercicio1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <titlte>Ejercicio 1</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1>Ejercicio1</h1>
+        <p>Aknaceba en un array los 10 primeros números pares. Imprímelos cada uno en una línea.</p>
+        <?php
+        
+        ?>
+    </body>
+</html>
