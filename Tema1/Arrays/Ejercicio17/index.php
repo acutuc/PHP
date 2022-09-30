7311662f81cdbee7e9551ffeb9aa906eeee8a16a
acutuc <acutuc94@gmail.com>
2022-09-30 09:27:58 +0200

    Subido ej17 y 19. Revisar 17

---
+++ Tema1/Arrays/Ejercicio17/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 17</title>
+</head>
+<body>
+    <h1>Ejercicio 17</h1>
+    
+    <?php
+        $familias = array("Los Simpson"=>array("Padre"=>"Homer", "Madre"=>"Marge", "Hijos"=>array("Bart", "Lisa", "Maggie")), "Los Griffin"=>array("Padre"=>"Peter", "Madre"=>"Lois", "Hijos"=>array("Chris", "Meg", "Stewie")));
+
+        echo "<ul>";
+        foreach($familias as $nombre_familia => $familia){
+            echo "<li>";
+                echo $nombre_familia;
+                echo "<ul>";
+                foreach($nombre_familia as $parentesco => $nombre){
+                    echo "<li>";
+                    if($parentesco == "Hijos"){
+                        echo $parentesco.": ";
+                        echo "<ol>";
+                        for($i = 0; count($nombre); $i++){
+                            echo "<li>";
+                            echo $nombre[$i];
+                            echo "</li>";
+                        }
+                        echo "</ol>";
+                    }else {
+                        echo $parentesco.": ".$valor;
+                    }
+                        
+                    echo "</li>";
+                }
+                echo "</ul>";
+            echo "</li>";
+        }
+        echo "</ul>";
+    ?>
+</body>
+</html>
