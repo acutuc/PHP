06d06a856e5a267d11e5cadf98de71e282926810
acutuc <acutuc94@gmail.com>
2022-10-28 10:11:45 +0200

    Terminar ejercicio 3

---
+++ Tema1/ExamenRepaso2/Ejercicio3/ejercicio3.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <title>Ejercicio 3</title>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+    <p>
+        Crea dos ficheros de texto, uno con nombre decodificado.txt que esté
+        vacío y otro con nombre codificado.txt que contengan las siguientes tres
+        líneas de texto:
+    </p>
+    <p>
+        JZ TLREUF KVIDZEVJ VC VAVITZTZF VJKR WIRJV KZVEV JVEKZUF.
+        VJKRIRJ ZXLRC UV WVCZQ HLV WVCZO.
+        WVCZO WLV LE RCLDEF HLV JRTF DLP SLVER EFKR.
+    </p>
+    <p>
+        Crea una página php, con nombre ejercicio3.php que permita
+        decodificar codificado.txt, en decodificado.txt, sabiendo que
+        codificado.txt, se encriptó empleando la codificación CESAR con una
+        clave desconocida. Se sabe que el fichero decodificado contiene la
+        palabra FELIX como parte de su texto.
+    </p>
+    <p>
+        El fichero decodificado.txt contendrá una última línea con la fecha en la
+        que el archivo fue decodificado:
+        “ Este fichero fue decodificado el Miércoles, día 30 de octubre de 2013 a
+        las 11:17 horas. ”.
+    </p>
+</body>
+
+</html>
