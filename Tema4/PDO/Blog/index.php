4a304fb99bfac7ff33369626c1c02dec2759b88b
acutuc <acutuc94@gmail.com>
2023-01-16 12:54:17 +0100

    Corregir y terminar

--- Tema4/PDO/Blog/index.php
+++ Tema4/PDO/Blog/index.php
@@ -1,14 +1,79 @@
 <?php
-require "src/config_bd.php";
-
 session_name("Blog_Curso22_23");
 session_start();
+
+require "src/config_bd.php";
+
+function error_page($title, $encabezado, $mensaje)
+{
+    return '<!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>' . $title . '</title>
+    </head>
+    <body>
+        <h1>' . $encabezado . '</h1>
+        ' . $mensaje . '
+    </body>
+    </html>';
+}
+
+//Si hay sesión (de cualquier campo):
+if(isset($_SESSION["usuario"])){
+    try {
+        $conexion = new PDO("mysql:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+    } catch (PDOException $e) {
+        die(error_page("Login PDO", "Login con PDO", "<p>Imposible conectar. Error: " . $e->getMessage() . "</p></body></html>"));
+    }
+}
 
 if (isset($_POST["btnEntrar"])) {
     $error_usuario = $_POST["usuario"] == "";
     $error_clave = $_POST["clave"] == "";
 
     $error_formulario = $error_usuario || $error_clave;
+
+    //Si no hay error en el formulario, conectamos...
+    if (!$error_formulario) {
+        //Conectamos a la BD.
+        try {
+            $conexion = new PDO("mysql:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+        } catch (PDOException $e) {
+            die(error_page("Blog Personal", "Blog Personal", "<p>Imposible conectar. Error: " . $e->getMessage() . "</p></body></html>"));
+        }
+    }
+
+    //... y miramos si existe el usuario: 
+    try {
+        $consulta = "SELECT * FROM usuarios WHERE usuario = ? AND clave = ?";
+
+        $sentencia = $conexion->prepare($consulta);
+
+        $datos[] = $_POST["usuario"];
+        $datos[] = md5($_POST["clave"]);
+
+        $sentencia->execute($datos);
+
+        //Si se ha logeado, creamos los $_SESSION
+        if ($sentencia->rowCount() > 0) {
+            $_SESSION["usuario"] = $datos[0];
+            $_SESSION["clave"] = $datos[1];
+            $_SESSION["ultimo_acceso"] = time();
+
+            header("Location:index.php");
+            exit();
+        } else {
+            //Si no se ha logeado, ponemos a true el $error_usuario:
+            $error_usuario = true;
+        }
+    } catch (PDOException $e) {
+        $sentencia = null;
+        $conexion = null;
+        die(error_page("Blog Personal", "Blog Personal", "<p>Imposible conectar. Error: " . $e->getMessage() . "</p></body></html>"));
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -31,8 +96,12 @@ if (isset($_POST["btnEntrar"])) {
             <input type="text" id="usuario" name="usuario" value="<?php if (isset($_POST["usuario"]))
                 echo $_POST["usuario"] ?>">
             <?php
-            if (isset($_POST["usuario"]) && $error_usuario) {
-                echo "<span class='error'>*Campo vacío*</span>";
+            if (isset($_POST["btnEntrar"]) && $error_usuario) {
+                if ($_POST["usuario"] == "") {
+                    echo "<span class='error'>*Campo vacío*</span>";
+                } else {
+                    echo "<span class='error'>*Usuario o contraseña mal escritos*</span>";
+                }
             }
             ?>
         </p>
@@ -40,7 +109,7 @@ if (isset($_POST["btnEntrar"])) {
             <label for="clave">Contraseña: </label>
             <input type="password" id="clave" name="clave">
             <?php
-            if (isset($_POST["clave"]) && $error_clave) {
+            if (isset($_POST["btnEntrar"]) && $error_clave) {
                 echo "<span class='error'>*Campo vacío*</span>";
             }
             ?>
@@ -60,18 +129,18 @@ if (isset($_POST["btnEntrar"])) {
 
     //2. Hacemos la consulta para mostrar las noticias.
     try {
-        $consulta = "SELECT * FROM noticias";
+        $consulta = "SELECT * FROM noticias ORDER BY noticias.fPublicacion DESC";
 
         $sentencia = $conexion->prepare($consulta);
         $sentencia->execute();
         $respuesta = $sentencia->fetchAll(PDO::FETCH_ASSOC);
 
-        if($sentencia->rowCount() > 0){//Si hemos obtenido tuplas:
+        if ($sentencia->rowCount() > 0) { //Si hemos obtenido tuplas:
             echo "<h2>Noticias</h2>";
 
-            foreach($respuesta as $tupla){
-                echo "<button type='submit' name='btnNoticia' class='noticia'>".$tupla["titulo"]."</button>";
-                echo "<p>".$tupla["copete"]."</p>";
+            foreach ($respuesta as $tupla) {
+                echo "<button type='submit' name='btnNoticia' class='noticia'>" . $tupla["titulo"] . "</button>";
+                echo "<p>" . $tupla["copete"] . "</p>";
                 echo "<br>";
             }
         }
--- Tema4/PDO/Blog/registro_usuario.php
+++ Tema4/PDO/Blog/registro_usuario.php
@@ -1,16 +1,37 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog Personal</title>
 </head>
+
 <body>
-    <h1>hola</h1>
-    Nombre
-    usuario
-    constraseña
-    email
-</body>
-</html>
+    <h1>Nuevo usuario</h1>
+    <form action="registro_usuario.php" method="post">
+        <p>
+            <label for="nombre"><strong>Nombre: </strong></label><input type="text" id="nombre" name="nombre"
+                value="<?php if (isset($_POST["nombre"]))
+                    echo $_POST["nombre"] ?>">
+            </p>
+            <p>
+                <label for="nombre"><strong>Usuario: </strong></label><input type="text" id="usuario" name="usuario"
+                    value="<?php if (isset($_POST["usuario"]))
+                    echo $_POST["usuario"] ?>">
+            </p>
+            <p>
+                <label for="nombre"><strong>Contraseña: </strong></label><input type="password" id="clave" name="clave">
+            </p>
+            <p>
+                <label for="nombre"><strong>Nombre: </strong></label><input type="email" id="email" name="email"
+                    value="<?php if (isset($_POST["email"]))
+                    echo $_POST["email"] ?>">
+            </p>
+            <button type="submit" formaction="index.php">Atrás</button>
+            <button type="submit" name="btnContinuar">Continuar</button>
+        </form>
+    </body>
+
+    </html>
